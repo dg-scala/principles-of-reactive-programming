55f3fe376c70eadefe3ca11ad869d158844e06a8
Dragan Glumac <dragan.glumac@gmail.com>
2015-04-21 16:33:38 +0100

    Polynomials done.

--- week2/calculator/src/main/scala/calculator/Polynomial.scala
+++ week2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,30 @@
 package calculator
 
+import scala.math._
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
-  }
+      c: Signal[Double]): Signal[Double] =
+    Signal(pow(b(), 2) - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+
+    def roots: Set[Double] = {
+      val d = delta()
+      if (d < 0) {
+        Set()
+      }
+      if (d == 0) {
+        Set() + (-b() / (2 * a()))
+      }
+      else {
+        Set() + (
+          (-b() - sqrt(d)) / (2 * a()),
+          (-b() + sqrt(d)) / (2 * a()))
+      }
+    }
+
+    Signal(roots)
   }
 }
