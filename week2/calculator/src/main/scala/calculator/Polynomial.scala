5a5f37bd70b4a7a88cfdf51a5802de242c379dc1
Dragan Glumac <dragan.glumac@gmail.com>
2015-04-26 12:20:36 +0100

    With minor rearrangement 10 out of 10, no real difference in the code logic though... Interesting :)

--- week2/calculator/src/main/scala/calculator/Polynomial.scala
+++ week2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -4,27 +4,30 @@ import scala.math._
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] =
+                   c: Signal[Double]): Signal[Double] =
     Signal(pow(b(), 2) - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
 
-    def roots: Set[Double] = {
-      val d = computeDelta(a, b, c)
-      if (d() < 0) {
-        Set()
+    def roots(): Set[Double] = {
+      val _d = delta()
+      val _a = a()
+      val _b = b()
+
+      if (_d == 0) {
+        Set() + (-_b / (2 * _a))
       }
-      if (d() == 0) {
-        Set() + (-b() / (2 * a()))
+      else if (_d > 0) {
+        Set() +(
+          (-_b - sqrt(_d)) / (2 * _a),
+          (-_b + sqrt(_d)) / (2 * _a))
       }
       else {
-        Set() + (
-          (-b() - sqrt(d())) / (2 * a()),
-          (-b() + sqrt(d())) / (2 * a()))
+        Set()
       }
     }
 
-    Signal(roots)
+    Signal(roots())
   }
 }
