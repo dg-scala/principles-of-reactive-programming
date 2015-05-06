16349260d2369f17b7ac129e36e3fd57fcf7a9ec
Dragan Glumac <dragan.glumac@gmail.com>
2015-05-06 14:41:02 +0100

    Additional  tests, that lo and behold made the same code get 10/10 where it had 8/10 before. Bizarre! Anyways, I am not complaining.

--- week3/nodescala/src/test/scala/nodescala/tests.scala
+++ week3/nodescala/src/test/scala/nodescala/tests.scala
@@ -124,7 +124,30 @@ class NodeScalaSuite extends FunSuite {
     Thread.sleep(1100)
     assert(count == 4)
   }
+  test("Server should be stoppable if receives infinite  response") {
+    val dummy = new DummyServer(8191)
+    val dummySubscription = dummy.start("/testDir") {
+      request => Iterator.continually("a")
+    }
 
+    // wait until server is really installed
+    Thread.sleep(500)
+
+    val webpage = dummy.emit("/testDir", Map("Any" -> List("thing")))
+    try {
+      // let's wait some time
+      Await.result(webpage.loaded.future, 1 second)
+      fail("infinite response ended")
+    } catch {
+      case e: TimeoutException =>
+    }
+
+    // stop everything
+    dummySubscription.unsubscribe()
+    Thread.sleep(500)
+    webpage.loaded.future.now // should not get NoSuchElementException
+  }
+  
   class DummyExchange(val request: Request) extends Exchange {
     @volatile var response = ""
     val loaded = Promise[String]()
