ca6768e5531d126b11a02b5c2e9c8f6bc1df477d
draganglumac <dragan.glumac@gmail.com>
2015-05-10 17:11:43 +0100

    Adding latest project to the repo.

--- week4/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ week4/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -6,19 +6,19 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.JavaConverters._
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Try, Success, Failure }
+import scala.util.{Try, Success, Failure}
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.Observable
+import rx.lang.scala.{Subscription, Observable}
 
 /** Basic facilities for dealing with Swing-like components.
-*
-* Instead of committing to a particular widget implementation
-* functionality has been factored out here to deal only with
-* abstract types like `ValueChanged` or `TextField`.
-* Extractors for abstract events like `ValueChanged` have also
-* been factored out into corresponding abstract `val`s.
-*/
+  *
+  * Instead of committing to a particular widget implementation
+  * functionality has been factored out here to deal only with
+  * abstract types like `ValueChanged` or `TextField`.
+  * Extractors for abstract events like `ValueChanged` have also
+  * been factored out into corresponding abstract `val`s.
+  */
 trait SwingApi {
 
   type ValueChanged <: Event
@@ -53,16 +53,16 @@ trait SwingApi {
       */
     def textValues: Observable[String] = ???
 
-  }
+    implicit class ButtonOps(button: Button) {
 
-  implicit class ButtonOps(button: Button) {
+      /** Returns a stream of button clicks.
+        *
+        * @param field the button
+        * @return an observable with a stream of buttons that have been clicked
+        */
+      def clicks: Observable[Button] = ???
+    }
 
-    /** Returns a stream of button clicks.
-     *
-     * @param field the button
-     * @return an observable with a stream of buttons that have been clicked
-     */
-    def clicks: Observable[Button] = ???
   }
 
 }
